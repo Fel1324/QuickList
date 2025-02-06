eaab0ca37a32c1b2ccecac5f025489df1c630fd8
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-02-06 14:55:54 -0300

    add delete item feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const formList = document.querySelector("#formList");
 const newItemInput = document.querySelector("#newItemInput");
 const shoppingList = document.querySelector("#shoppingList");
+const message = document.querySelector("#alert");
+const closeMessage = document.querySelector("#closeAlert");
 let itemId = 1;
 
 newItemInput.addEventListener("input", () => {
@@ -15,25 +17,49 @@ formList.addEventListener("submit", (event) => {
     createNewItem(itemId, newItemInput.value);
     itemId += 1;
 
+    newItemInput.value = "";
+    newItemInput.focus();
+
   } else {
     alert("O item deve possuir um nome!");
   }
 });
 
 function createNewItem(id, inputValue){
+  // CRIA ELEMENTO LI, ADICIONA CONTEÚDO, ATRIBUE CLASSE E ID.
   let newItem = document.createElement("li");
   newItem.innerHTML += `
     <div>
       <input type="checkbox" name="checkboxItem" id=${"checkboxItem" + id}>
       <label for=${"checkboxItem" + id}>${inputValue}</label>
     </div>
-
-    <button id="delete" aria-label="Deletar item">
-      <img src="./assets/icons/delete.svg" alt="">
-    </button>
   `;
   newItem.classList.add("shoppingItem");
   newItem.id = id;
 
+  // CRIA ELEMENTO  BUTTON, ADICIONA CONTEÚDO, ATRIBUE ARIA-LABEL E FUNÇÃO DE DELETAR.
+  let btnItem = document.createElement("button");
+  btnItem.innerHTML += `
+    <img src="./assets/icons/delete.svg" alt="">
+  `;
+  btnItem.setAttribute("aria-label", "Deletar item");
+  btnItem.onclick = () => deleteItem(id);
+
+  // ADICIONA BUTTON NA LI E LI NA UL.
+  newItem.appendChild(btnItem);
   shoppingList.append(newItem);
 }
+
+function deleteItem(id){
+  document.getElementById(id).remove();
+
+  message.classList.add("show-alert");
+
+  setTimeout(() => {
+    message.classList.remove("show-alert");
+  }, 2500);
+}
+
+closeMessage.addEventListener("click", () => {
+  message.classList.remove("show-alert");
+});
